6c8697431a0fc864e6a944d516ab1bbdc0d7c07f
Artem Tumanov <artemon002@gmail.com>
2021-05-05 16:31:51 +0300

    Added links for trades

--- src/view/components/Trades/Trades.tsx
+++ src/view/components/Trades/Trades.tsx
@@ -1,16 +1,22 @@
 import React, { FC } from 'react';
 import { useSelector } from 'react-redux';
+import { Link, useRouteMatch } from 'react-router-dom';
 import { IStoreState } from 'src/interfaces/store';
 import { TradeCard } from '../TradeCard';
 import './Trades.style.css';
 
 export const Trades : FC = () => {
+  const match = useRouteMatch();
   const currentTrade = useSelector((state : IStoreState) => state.trades.currentTrade);
   const trades = useSelector((state: IStoreState) => state.trades.data);
   
   return (
     <div className="trades">
-      {trades.map(trade => <TradeCard key={trade.id} trade={trade} currentTrade={currentTrade} />)}
+      {trades.map(trade => (
+        <Link to={`${match.url}/${trade.id}`}>
+          <TradeCard key={trade.id} trade={trade} currentTrade={currentTrade} />
+        </Link>
+      ))}
     </div>
   )
 };
